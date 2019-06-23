45f3f2b7433926cff9b9cd66ad72ece7472ca332
Franck Nijhof <frenck@addons.community>
2019-06-23 18:47:44 +0200

    :sparkles: Adds Ingress support

--- grocy/rootfs/etc/cont-init.d/nginx.sh
+++ grocy/rootfs/etc/cont-init.d/nginx.sh
@@ -5,6 +5,8 @@
 # ==============================================================================
 declare certfile
 declare keyfile
+declare ingress_interface
+declare ingress_entry
 
 bashio::config.require.ssl
 
@@ -18,3 +20,9 @@ if bashio::config.true 'ssl'; then
 else
     mv /etc/nginx/servers/direct.disabled /etc/nginx/servers/direct.conf
 fi
+
+ingress_interface=$(bashio::addon.ip_address)
+sed -i "s/%%interface%%/${ingress_interface}/g" /etc/nginx/servers/ingress.conf
+
+ingress_entry=$(bashio::addon.ingress_entry)
+sed -i "s#%%ingress_entry%%#${ingress_entry}#g" /etc/php7/php-fpm.d/ingress.conf

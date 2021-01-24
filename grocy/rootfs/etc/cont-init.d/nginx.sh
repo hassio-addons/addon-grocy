f720ccb0e38bcddee27e5e6aa03d15f36b96d73a
Franck Nijhof <frenck@addons.community>
2021-01-24 16:38:00 +0100

    Refactor NGINX/PHP-FPM configuration (#115)
    * Refactor NGINX/PHP-FPM configuration
    
    * Use default ingress port

--- grocy/rootfs/etc/cont-init.d/nginx.sh
+++ grocy/rootfs/etc/cont-init.d/nginx.sh
@@ -3,34 +3,22 @@
 # Home Assistant Community Add-on: Grocy
 # Configures NGINX for use with Grocy
 # ==============================================================================
-declare certfile
-declare dns_host
-declare ingress_entry
-declare ingress_interface
-declare keyfile
-declare port
 
-port=$(bashio::addon.port 80)
-if bashio::var.has_value "${port}"; then
+# Generate Ingress PHP-FPM configuration
+bashio::var.json \
+    interface "$(bashio::addon.ip_address)" \
+    | tempio \
+        -template /etc/nginx/templates/ingress.gtpl \
+        -out /etc/nginx/servers/ingress.conf
+
+# Generate direct access configuration, if enabled.
+if bashio::var.has_value "$(bashio::addon.port 80)"; then
     bashio::config.require.ssl
-
-    if bashio::config.true 'ssl'; then
-        certfile=$(bashio::config 'certfile')
-        keyfile=$(bashio::config 'keyfile')
-
-        mv /etc/nginx/servers/direct-ssl.disabled /etc/nginx/servers/direct.conf
-        sed -i "s#%%certfile%%#${certfile}#g" /etc/nginx/servers/direct.conf
-        sed -i "s#%%keyfile%%#${keyfile}#g" /etc/nginx/servers/direct.conf
-    else
-        mv /etc/nginx/servers/direct.disabled /etc/nginx/servers/direct.conf
-    fi
+    bashio::var.json \
+        certfile "$(bashio::config 'certfile')" \
+        keyfile "$(bashio::config 'keyfile')" \
+        ssl "^$(bashio::config 'ssl')" \
+        | tempio \
+            -template /etc/nginx/templates/direct.gtpl \
+            -out /etc/nginx/servers/direct.conf
 fi
-
-ingress_interface=$(bashio::addon.ip_address)
-sed -i "s/%%interface%%/${ingress_interface}/g" /etc/nginx/servers/ingress.conf
-
-ingress_entry=$(bashio::addon.ingress_entry)
-sed -i "s#%%ingress_entry%%#${ingress_entry}#g" /etc/php7/php-fpm.d/ingress.conf
-
-dns_host=$(bashio::dns.host)
-sed -i "s/%%dns_host%%/${dns_host}/g" /etc/nginx/includes/resolver.conf

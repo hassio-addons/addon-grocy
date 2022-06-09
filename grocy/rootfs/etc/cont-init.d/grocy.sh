35d0fa823e3c18f7bb320362c587997bbf5c906a
Franck Nijhof <git@frenck.dev>
2022-06-09 22:26:18 +0200

    :ambulance: Fix Grocy startup issues (#301)

--- grocy/rootfs/etc/cont-init.d/grocy.sh
+++ grocy/rootfs/etc/cont-init.d/grocy.sh
@@ -8,11 +8,6 @@ if ! bashio::fs.directory_exists "/data/grocy"; then
 
     # Setup structure
     cp -R /var/www/grocy/data /data/grocy
-
-    # Ensure file permissions
-    chown -R nginx:nginx /data/grocy
-    find /data/grocy -not -perm 0644 -type f -exec chmod 0644 {} \;
-    find /data/grocy -not -perm 0755 -type d -exec chmod 0755 {} \;
 fi
 
 if ! bashio::fs.directory_exists "/data/grocy/viewcache"; then

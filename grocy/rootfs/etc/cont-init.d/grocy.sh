21bf1623e27dff903d5f9a78b9ca092199554829
Franck Nijhof <git@frenck.dev>
2022-06-09 10:47:31 +0200

    ⬆️ Upgrades add-on base image to 12.0.0 (#298)

--- grocy/rootfs/etc/cont-init.d/grocy.sh
+++ grocy/rootfs/etc/cont-init.d/grocy.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/with-contenv bashio
+#!/command/with-contenv bashio
 # ==============================================================================
 # Home Assistant Community Add-on: Grocy
 # Sets up Grocy before the server starts
